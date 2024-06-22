a0489d64dbcbdb96b25c771e317b432f641d99c4
elenamurgia <145214693+elenamurgia@users.noreply.github.com>
2024-06-22 23:08:49 +0100

    reverts the changes made previously to the listen.js file

--- listen.js
+++ listen.js
@@ -1,11 +1,6 @@
 const app = require("./app.js");
-const { PORT = 10000 } = process.env;
+const { PORT = 9090 } = process.env;
 
 console.log(`PORT environment variable: ${PORT}`);
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
-
-// const app = require("./app.js");
-// const port = process.env.PORT || 9090;
-
-// app.listen(port, () => console.log(`Listening on ${port}...`));
