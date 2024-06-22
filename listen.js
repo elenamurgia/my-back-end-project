c74c66171a39fdb5df70e603e800a32fe576b09f
elenamurgia <145214693+elenamurgia@users.noreply.github.com>
2024-06-22 22:56:20 +0100

    makes changes to listen.js to try to fix an issue with the hostin server

--- listen.js
+++ listen.js
@@ -1,4 +1,9 @@
-const app = require("./app.js");
-const { PORT = 9090 } = process.env;
+// const app = require("./app.js");
+// const { PORT = 9090 } = process.env;
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+// app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+
+const app = require("./app.js");
+const port = 9090 = process.env.PORT || 9090;
+
+app.listen(port, () => console.log(`Listening on ${port}...`));
